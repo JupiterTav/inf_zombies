5d9d8f5ec3bcc40c014b0e684133eb806aeea8d7
JupiterTav <wesleyTavaresDev@outlook.com>
2025-06-11 00:14:27 -0300

    outline and changed slice struct to Rectangle(raylib)

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include <raymath.h>
+#include <string.h>
 
 #define GRID_COLUMS 9
 #define GRID_LINES 5
@@ -12,12 +13,14 @@ typedef struct slice {
 }block;
 
 struct Grid{
-  block map[GRID_LINES][GRID_COLUMS];
-  block *selected_block;
+  Rectangle map[GRID_LINES][GRID_COLUMS];
+  Rectangle *selected_block;
 };
 
 void initialize_grid(struct Grid *grid, float x, float y, float width, float height);
 void draw_grid(struct Grid *grid);
+void draw_outlined_selected_grid(struct Grid *grid);
+void manage_selected_grid(struct Grid *grid);
 
 int main() {
   struct Grid game_grid;
@@ -26,7 +29,13 @@ int main() {
   InitWindow(978, 640, "Inf_zombies");
 
   while(!WindowShouldClose()){ 
+    manage_selected_grid(&game_grid);
+
+    BeginDrawing();
+    ClearBackground(SKYBLUE); 
     draw_grid(&game_grid);
+    draw_outlined_selected_grid(&game_grid);
+    EndDrawing();
   }
   CloseWindow();
   return 0;
@@ -35,25 +44,41 @@ int main() {
 void initialize_grid(struct Grid *grid, float x, float y, float width, float height){
     for(int i = 0; i < GRID_LINES; i++) {
       for(int j = 0; j < GRID_COLUMS; j++){
-      grid->map[i][j].center.x = x;
-      grid->map[i][j].center.y = y;
-      grid->map[i][j].dimensions.x  = width ;
-      grid->map[i][j].dimensions.y = height;
+        grid->map[i][j].x = x + (72 * j);
+        grid->map[i][j].y = y + (92*i);
+        grid->map[i][j].width  = width ;
+        grid->map[i][j].height = height;
+      }
+    }
+
+    grid->selected_block = &grid->map[0][0];
+}
+
+void draw_grid(struct Grid *grid) {
+  Color slice_color = GREEN;
+  for(int i = 0; i < GRID_LINES; i++){
+    for(int j = 0; j < GRID_COLUMS; j++){
+      slice_color = ColorIsEqual(slice_color, GREEN) == 1 ? BROWN : GREEN;
+      DrawRectangle( grid->map[i][j].x, grid->map[i][j].y,
+          grid->map[i][j].width, grid->map[i][j].height, slice_color);
     }
   }
 }
 
-void draw_grid(struct Grid *grid) {
-  BeginDrawing();
-  Color slice_color = GREEN;
-  for(int i = 0; i < GRID_LINES; i++){
-    for(int j = 0; j < GRID_COLUMS; j++){
-      slice_color = ColorIsEqual(slice_color, GREEN) == 1 ? DARKBROWN : GREEN;
-      DrawRectangle( grid->map[i][j].center.x + (72*j), grid->map[i][j].center.y + (92*i),
-          grid->map[i][j].dimensions.x, grid->map[i][j].dimensions.y, slice_color);
-    }
+void manage_selected_grid(struct Grid *grid){
+  if(IsKeyPressed(KEY_RIGHT)){
+    if(grid->selected_block != &(grid->map[GRID_LINES - 1][GRID_COLUMS - 1]))
+        grid->selected_block++;
+    else 
+        grid->selected_block = &(grid->map[0][0]);
   }
-  EndDrawing();
+}
+
+void draw_outlined_selected_grid(struct Grid *grid){
+  Rectangle rect;
+  //DrawRectangleLines(grid->selected_block->center.x, grid->selected_block->center.y, 
+   //   grid->selected_block->dimensions.x, grid->selected_block->dimensions.y, BLACK);
+  DrawRectangleLinesEx(*(grid->selected_block), 2, BLACK);
 }
 
 
