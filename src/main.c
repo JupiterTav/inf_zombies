de17fcd49331fb3f87d047f0b0d2aac57db76354
JupiterTav <wesleyTavaresDev@outlook.com>
2025-06-12 10:13:39 -0300

    outline and the block selection almost done

--- src/main.c
+++ src/main.c
@@ -1,20 +1,18 @@
 #include <raylib.h>
 #include <raymath.h>
-#include <string.h>
 
 #define GRID_COLUMS 9
 #define GRID_LINES 5
 #define GRID_START 20
 
-typedef struct slice {
+typedef struct Block {
   int theres_plant;
-  Vector2 center;
-  Vector2 dimensions;
-}block;
+  Rectangle tile;
+}Slice;
 
 struct Grid{
-  Rectangle map[GRID_LINES][GRID_COLUMS];
-  Rectangle *selected_block;
+  Slice map[GRID_LINES][GRID_COLUMS];
+  Slice *selected_block;
 };
 
 void initialize_grid(struct Grid *grid, float x, float y, float width, float height);
@@ -44,10 +42,10 @@ int main() {
 void initialize_grid(struct Grid *grid, float x, float y, float width, float height){
     for(int i = 0; i < GRID_LINES; i++) {
       for(int j = 0; j < GRID_COLUMS; j++){
-        grid->map[i][j].x = x + (72 * j);
-        grid->map[i][j].y = y + (92*i);
-        grid->map[i][j].width  = width ;
-        grid->map[i][j].height = height;
+        grid->map[i][j].tile.x = x + (72 * j);
+        grid->map[i][j].tile.y = y + (92*i);
+        grid->map[i][j].tile.width  = width ;
+        grid->map[i][j].tile.height = height;
       }
     }
 
@@ -59,26 +57,52 @@ void draw_grid(struct Grid *grid) {
   for(int i = 0; i < GRID_LINES; i++){
     for(int j = 0; j < GRID_COLUMS; j++){
       slice_color = ColorIsEqual(slice_color, GREEN) == 1 ? BROWN : GREEN;
-      DrawRectangle( grid->map[i][j].x, grid->map[i][j].y,
-          grid->map[i][j].width, grid->map[i][j].height, slice_color);
+      DrawRectangle( grid->map[i][j].tile.x, grid->map[i][j].tile.y,
+          grid->map[i][j].tile.width, grid->map[i][j].tile.height, slice_color);
     }
   }
 }
 
 void manage_selected_grid(struct Grid *grid){
+  static int current_line = 0;
   if(IsKeyPressed(KEY_RIGHT)){
     if(grid->selected_block != &(grid->map[GRID_LINES - 1][GRID_COLUMS - 1]))
         grid->selected_block++;
     else 
         grid->selected_block = &(grid->map[0][0]);
   }
+
+  else if(IsKeyPressed(KEY_LEFT)){
+    if(grid->selected_block != &(grid->map[0][0]))
+        grid->selected_block--;
+    else
+        grid->selected_block = &(grid->map[GRID_LINES - 1][GRID_COLUMS - 1]);
+  }
+  
+  else if(IsKeyPressed(KEY_DOWN)){
+    if(current_line < 4){
+      grid->selected_block += 9;
+      current_line++;
+    }
+    else { 
+      grid->selected_block -= (36 /  current_line);
+      current_line = 0;
+    }
+  }
+  else if(IsKeyPressed(KEY_UP)){    
+    if(current_line > 0){
+      grid->selected_block -= 9;
+      current_line--;
+    }
+    else{
+      grid->selected_block += 36;
+      current_line = 4;
+    }
+  }
 }
 
 void draw_outlined_selected_grid(struct Grid *grid){
-  Rectangle rect;
-  //DrawRectangleLines(grid->selected_block->center.x, grid->selected_block->center.y, 
-   //   grid->selected_block->dimensions.x, grid->selected_block->dimensions.y, BLACK);
-  DrawRectangleLinesEx(*(grid->selected_block), 2, BLACK);
+  DrawRectangleLinesEx((grid->selected_block->tile), 5, BLACK);
 }
 
 
